f05cd6c2682d4f17ef5a0bdb9d84c5aeef5c7463
Ohjanghoon <ojh4105@naver.com>
2025-01-15 18:48:10 +0900

    [refactor] firebase 설정 파일 추가, gitignore (.env 추가) 수정

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_firebase_apiKey,
+  authDomain: process.env.REACT_APP_firebase_authDomain,
+  projectId: process.env.REACT_APP_firebase_projectId,
+  storageBucket: process.env.REACT_APP_firebase_storageBucket,
+  messagingSenderId: process.env.REACT_APP_firebase_messagingSenderId,
+  appId: process.env.REACT_APP_firebase_appId,
+  measurementId: process.env.REACT_APP_firebase_measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default analytics;
