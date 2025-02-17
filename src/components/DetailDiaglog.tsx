9bddf0ec274e05c22aa0f62ff0ca33da1cfe47fb
Ohjanghoon <ojh4105@naver.com>
2025-02-18 02:37:05 +0900

    [feat] ESC key 다운시 다이얼로그 클로즈 이벤트 추가

--- src/components/DetailDiaglog.tsx
+++ src/components/DetailDiaglog.tsx
@@ -18,6 +18,7 @@ import { ProjectItem } from "../types/projectTypes";
 
 // styles
 import "swiper/swiper-bundle.css";
+import { useEffect } from "react";
 // import "swiper/css/free-mode";
 // import "swiper/css/navigation";
 // import "swiper/css/pagination";
@@ -34,6 +35,18 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
     document.body.style.overflow = "auto";
   };
 
+  useEffect(() => {
+    const escKeyDownHandler = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", escKeyDownHandler);
+    return () => {
+      window.removeEventListener("keydown", escKeyDownHandler);
+    };
+  }, []);
+
   return (
     <AnimatePresence>
       <motion.div
@@ -43,7 +56,7 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
         exit={{ opacity: 0 }}
       >
         <motion.div
-          className="relative h-[80vh] w-[90vw] overflow-scroll rounded-xl bg-white px-4 py-10 shadow-lg md:max-w-4xl md:px-14 2xl:max-w-6xl"
+          className="dark:bg-dark-surface relative h-[80vh] w-[90vw] overflow-scroll rounded-xl bg-white px-4 py-10 shadow-lg md:max-w-4xl md:px-14 2xl:max-w-6xl"
           initial={{ scale: 0.8, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           exit={{ scale: 0.8, opacity: 0 }}
@@ -52,19 +65,22 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
           <div className="p-3">
             {/* 프로젝트 타입 */}
             <span
-              className={item.type.color + ` rounded-full px-2 py-1 text-[9px]`}
+              className={
+                item.type.color +
+                ` dark:text-dark-text-dark rounded-full px-2 py-1 text-[9px]`
+              }
             >
               {item.type.name}
             </span>
 
             {/* 프로젝트 명 */}
-            <h3 className="h4 mt-1">{item.title}</h3>
+            <h4 className="mt-1">{item.title}</h4>
           </div>
           {/* 본문 영역 */}
-          <div className="text-sm leading-relaxed md:text-base">
+          <div className="mt-3 text-sm leading-relaxed md:text-base">
             {/* 이미지 슬라이드 영역 */}
             <div className="w-full px-3">
-              <div className="rounded-t-3xl border-[0.5px] border-text-secondary bg-gradient-to-tr from-[#f3f3f3] via-[#f1f1f1] to-[#f2f2f2] px-5 pt-5 md:px-8 md:pt-8">
+              <div className="rounded-t-3xl border-[0.25px] bg-[#090909] px-5 pt-5 md:px-6 md:pt-6">
                 {item.screenshots?.length > 0 ? (
                   <Swiper
                     loop={true}
@@ -112,36 +128,12 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
                   </Swiper>
                 ) : (
                   <div className="h-full w-full pb-8">
-                    <div className="inline-flex h-[160px] w-full flex-col items-center justify-center bg-zinc-400/60 text-text-light/60 md:h-[400px]">
+                    <div className="text-light-text-light/60 inline-flex h-[160px] w-full flex-col items-center justify-center bg-slate-400 md:h-[400px]">
                       <MdImageNotSupported className="mx-auto text-[6rem] md:text-[12rem]" />
-                      <h6 className="h6 text-center">No Image</h6>
+                      <h6 className="text-center">No Image</h6>
                     </div>
                   </div>
                 )}
-
-                {/* <Swiper
-              onSwiper={setThumbsSwiper}
-              loop={true}
-              spaceBetween={10}
-              slidesPerView={3}
-              freeMode={true}
-              watchSlidesProgress={true}
-              modules={[FreeMode, Navigation, Thumbs]}
-              className="mySwiper"
-            >
-              {item.screenshots?.map((screenshot, index) => {
-                return (
-                  <SwiperSlide key={index}>
-                    <img
-                      key={index}
-                      src={screenshot}
-                      alt=""
-                      className="w-12 h-12"
-                    />
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper> */}
               </div>
               <div className="flex h-12 items-center justify-center rounded-b-2xl border-[0.5px] border-text-secondary bg-[#cecece] md:h-16">
                 {/* 프로젝트 URL */}
@@ -150,17 +142,15 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
                     href={item.liveDemoUrl.url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex items-center justify-center gap-1 rounded p-2 text-xs shadow-neumorphism-pressed md:w-12 md:flex-col"
+                    className="shadow-light-neumorphism-pressed text-light-text-dark flex items-center justify-center gap-1 rounded p-2 text-xs md:w-12 md:flex-col"
                   >
                     <FaExternalLinkAlt />
                     <span>Demo</span>
                   </a>
                 ) : (
-                  <p className="break-keep px-2 text-center text-xs text-text-secondary md:text-sm">
+                  <p className="text-light-text-secondary break-keep px-2 text-center text-xs md:text-sm">
                     <span className="text-red-500">* </span>
                     {item.liveDemoUrl.expression}
-                    {/* 해당 프로젝트는 비공개 프로젝트로, 보안 정책에 따라 Live
-                    Demo를 제공할 수 없는 점 양해 부탁드립니다. */}
                   </p>
                 )}
               </div>
@@ -170,16 +160,16 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
             <div className="mt-6 inline-flex w-full flex-col gap-10 px-3 py-3">
               {/* 프로젝트 설명 */}
               <div>
-                <span className="h6 rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1">
+                <h6 className="inline rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1 dark:bg-accent-hover/30">
                   개요
-                </span>
+                </h6>
                 <p className="m-2 mt-3 tracking-tight">{item.description}</p>
               </div>
               {/* 프로젝트 기술 */}
               <div>
-                <span className="h6 rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1">
+                <h6 className="inline rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1 dark:bg-accent-hover/30">
                   사용 기술
-                </span>
+                </h6>
                 <div className="m-2 mt-3 flex flex-wrap gap-2 text-xs">
                   {item.technologies.map((tech, index) => (
                     <span
@@ -193,18 +183,18 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
               </div>
               {/* 프로젝트 참여 기간 */}
               <div>
-                <span className="h6 rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1">
+                <h6 className="inline rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1 dark:bg-accent-hover/30">
                   기간
-                </span>
+                </h6>
                 <p className="m-2 mt-3 tracking-tighter">
                   {item.duration.start} ~ {item.duration.end}
                 </p>
               </div>
               {/* 프로젝트 주요 개발 사항 */}
               <div>
-                <span className="h6 rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1">
+                <h6 className="inline rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1 dark:bg-accent-hover/30">
                   주요 개발 및 기여
-                </span>
+                </h6>
                 <ul className="m-2 mt-3 flex flex-col gap-4">
                   {item.responsibilities.map((task, index) => {
                     return (
@@ -232,12 +222,12 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
               {/* 프로젝트 성과 및 배운 점 */}
               {item.achievements?.length > 0 && (
                 <div>
-                  <span className="h6 rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1">
+                  <h6 className="inline rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1 dark:bg-accent-hover/30">
                     성과 및 배운 점
-                  </span>
+                  </h6>
                   <ul className="m-2 ml-5 mt-3 list-disc">
                     {item.achievements?.map((task, index) => (
-                      <li key={"arch" + index} className="my-1">
+                      <li key={"arch" + index} className="my-[6px]">
                         {task}
                       </li>
                     ))}
@@ -246,9 +236,9 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
               )}
               {/* Git 주소 */}
               <div>
-                <span className="h6 rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1">
+                <h6 className="inline rounded-[3px] border-l-4 border-accent-hover bg-accent-hover/10 px-2 py-1 dark:bg-accent-hover/30">
                   소스 코드 주소
-                </span>
+                </h6>
                 {item.repositoryUrl?.available ? (
                   <a
                     href={item.repositoryUrl.url}
@@ -259,7 +249,7 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
                     GitHub로 이동하기
                   </a>
                 ) : (
-                  <p className="m-2 mt-3 break-keep text-text-secondary">
+                  <p className="text-light-text-secondary m-2 mt-3 break-keep">
                     <span className="text-red-500">* </span>
                     {item.repositoryUrl?.expression}
                     {/* 해당 프로젝트는 비공개 프로젝트로, 보안 정책에 따라 Live
@@ -272,7 +262,7 @@ function DetailDialog({ data: item, handleOpenDialog }: Props) {
 
           {/* 닫기 버튼 영역 */}
           <button
-            className="absolute right-3 top-3 text-3xl text-gray-600 hover:text-accent-hover"
+            className="absolute right-3 top-3 text-3xl hover:text-accent-hover"
             onClick={onClose}
           >
             <IoIosCloseCircle />
