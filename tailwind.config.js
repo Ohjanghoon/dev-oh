c617d686cac7407da22e455630e424b1d1a67b49
Ohjanghoon <ojh4105@naver.com>
2025-02-18 02:35:34 +0900

    [refactor] tailwind config dark 모드 팔레트 추가 및 전반적인 테마 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,26 +21,29 @@ export default {
       },
       colors: {
         light: {
-          background: "#e5e7eb",
+          primary: "#e5e7eb",
+          surface: "#f4f4f5",
+          gradient: "#96adcd",
           text: {
-            DEFAULT: "#0b0c0e",
-            secondary: "#373d48",
+            DEFAULT: "#374151",
+            secondary: "#6b7280",
+            neutral: "#9ca3af",
+            light: "#fafafa",
+            dark: "#1c1c22",
+            hover: "#f5f5ff",
           },
         },
 
         dark: {
-          background: "#1c1c22",
+          primary: "#1c1c22",
+          surface: "#23232a",
+          gradient: "#2b3a55",
+          text: {
+            DEFAULT: "#f7f7f8",
+            dark: "#1c1c22",
+          },
         },
 
-        text: {
-          DEFAULT: "var(--text-gray-700)",
-          secondary: "#6b7280",
-          light: "#e5e7eb",
-        },
-
-        "text-dark": "#1c1c22",
-        "border-light": "#e5e7eb",
-        "border-dark": "#1c1c22",
         "border-accent": "#3a43ff",
         "border-hover": "#2438e5",
         accent: {
@@ -56,26 +59,15 @@ export default {
           // 900: "#0202b0",
           // 950: "#000778",
         },
-        "text-light": "#FAFAFA",
-        "text-hover": "#F5F5FF",
       },
       boxShadow: {
-        "light-neumorphism": "6px 6px 20px #a0a2a5, -6px -6px 20px #ffffff",
+        "light-neumorphism": "6px 6px 16px #a0a2a5, -6px -6px 16px #ffffff",
+        "light-neumorphism-pressed":
+          "inset 6px 6px 16px #a0a2a5, inset -6px -6px 16px #ffffff",
 
-        "dark-neumorphism": "8px 8px 8px #141418, -8px -8px 8px #24242c;",
+        "dark-neumorphism": "6px 6px 16px #141418, -6px -6px 16px #24242c;",
         "dark-neumorphism-pressed":
-          "inset 8px 8px 8px #141418, inset -8px -8px 8px #24242c;",
-
-        // neumorphism: "6px 6px 16px #a3a3a3, -6px -6px 16px #ffffff",
-        // "neumorphism-hover": "4px 4px 10px #a3a3a3, -4px -4px 10px #ffffff",
-        // "neumorphism-pressed":
-        //   "inset 4px 4px 8px #a3a3a3, inset -3px -3px 8px #ffffff",
-
-        // "neumorphism-blue": "2px 2px 10px #0000C5, -2px -2px 10px #a5b2ff",
-        // "neumorphism-blue-hover":
-        //   "4px 4px 10px #000080, -4px -4px 10px #4D4DFF",
-        // "neumorphism-blue-pressed":
-        //   "inset 4px 4px 10px #000080, inset -4px -4px 10px #4D4DFF",
+          "inset 6px 6px 16px #141418, inset -6px -6px 16px #24242c;",
       },
     },
   },
